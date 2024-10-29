69545a37a415e23a9c0d931bacb1088d55e74db4
Ardhyana22 <ardhyanapratamaputra@gmail.com>
2024-10-28 20:38:24 -1200

    push ke github

---
+++ latihan2.js
@@ -0,0 +1,11 @@
+// pembuatan variable
+var age = 20; // variabel global type number
+let name = "Buddy"; //variable type string
+const country = "Indonesia"; // variable type string 
+let isStudent = true; //boolean
+let person = {name: "Buddy", age: 20} //object
+let numbers = [1,2,3,4,5]; //array 
+//menampilkan type variable
+console.log(typeof age, typeof name, typeof country, typeof isStudent, typeof person, typeof numbers);
+//menampilkan isi variable
+console.log("nama:" +name, "\n", "umur:" +age, "\n", "negara:" +country );
---
+++ latihan3.js
@@ -0,0 +1,18 @@
+// arithmetic operators
+let sum = 10 + 5; //penjumlahan
+let sub = 10 - 5; //pengurangan
+let mul = 10 * 5; //perkalian
+let div = 10 / 5; //pembagian
+let mod = 10 % 5; //modulus
+let exp = 10 ** 5; //eksponen/peringkat
+console.log(sum, sub, mul, div, mod, exp);
+//comparison operators
+let isEqual = 5 === "5"; // sama dengan strict
+let isEqual2 = 5 == "5";  //sama dengan not strict
+let isNotEqual = 10 != "10"; // tidak sama dengan 1 not strict
+let isNotEqual2 = 10 !== "10"; //tidak sama dengan 2 strict
+let isGreater = 10 > 5; //lebih besar
+let isless = 5 < 10; //lebih kecil
+let isGreaterEqual = 10 >= 10; //lebih besar sama dengan
+let islessEqual = 5 <= 10;  //lebih kecil sama dengan
+console.log(isEqual, isEqual2, isNotEqual, isNotEqual2, isGreater, isless, isGreaterEqual, islessEqual);
